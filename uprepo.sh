19206da63ba9b1ebb4bc9996b87c685964357ecb
Admon <83048967+ifhuda@users.noreply.github.com>
2026-08-06 20:26:00 +0700

    Update uprepo.sh

--- uprepo.sh
+++ uprepo.sh
@@ -1,2 +1,9 @@
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
 sudo sh webmin-setup-repo.sh
+
+
+echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list.d/ddebs.list
+
+sudo apt install ubuntu-dbgsym-keyring

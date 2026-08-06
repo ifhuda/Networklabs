a967be5c4ec7b20b8f3c78840641781bd38da8ee
Ahmad Saiful Huda <83048967+mon-adm0n@users.noreply.github.com>
2026-08-06 15:21:16 +0700

    Create inital-basic.sh

---
+++ inital-basic.sh
@@ -0,0 +1,8 @@
+sudo apt -y update
+sudo apt -y install acl net-tools python3-dev python3-pip samba samba-client apache2 openssh-server
+pip3 install requests
+pip3 install sh
+pip3 install requests
+pip3 install netifaces
+pip3 install psutil
+pip3 install tornado
